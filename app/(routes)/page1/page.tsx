2b108dc2b0f3e13f8cef439f5998f09a744c4a56
ConsonanteC <pipejimenez.jfjr@gmail.com>
2024-08-12 07:49:53 -0500

    correction

--- app/(routes)/page1/page.tsx
+++ app/(routes)/page1/page.tsx
@@ -2,6 +2,7 @@ export const page1 = () => {
     return (
         <div>
             <h1>Página 1</h1>
+            <a href="/">Home</a>
         </div>
     )
 }
--- app/(routes)/page2/page.tsx
+++ app/(routes)/page2/page.tsx
@@ -2,6 +2,7 @@ const page2 = () => {
     return (
         <div>
             <h1>Página 2</h1>
+            <a href="/">Home</a>
         </div>
     )
 }
